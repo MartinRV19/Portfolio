82c517e1e6ddd0dde88685be66a9a509d7342944
mtn1497 <mtn1497@gmail.com>
2025-04-18 22:25:08 +0000

    c13

--- app/Project2/page.tsx
+++ app/Project2/page.tsx
@@ -5,7 +5,9 @@ import NotebookViewer from "@/components/notebook-viewer";
 const Project2Page = () => {
   return (
     <div className="container py-8 md:py-12">
-      <NotebookViewer notebookPath="/notebooks/venta_departamentos_properati.html" />
+      <div className="bg-card border rounded-lg overflow-hidden">
+        <NotebookViewer notebookPath="/notebooks/venta_departamentos_properati.html" />
+      </div>
     </div>
   );
 };
