4a38bd8988b5f91137dbfc726b32cb066a62e86e
mtn1497 <mtn1497@gmail.com>
2025-04-18 23:15:11 +0000

    c21

--- components/notebook-viewer.tsx
+++ components/notebook-viewer.tsx
@@ -65,7 +65,6 @@ const NotebookViewer: React.FC<NotebookViewerProps> = ({ notebookPath }) => {
       width="100%"
       height="800px"
       style={{
-        border: "none",
         width: "100%",
         height: "100%",
         overflow: "visible",
