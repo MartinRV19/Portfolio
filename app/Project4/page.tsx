8263db360ef3c5bcec23512554602c54482267d0
mtn1497 <mtn1497@gmail.com>
2025-04-25 19:38:47 +0000

    c39

--- app/Project4/page.tsx
+++ app/Project4/page.tsx
@@ -9,11 +9,13 @@ const HtmlRenderer = dynamic(
 
 export default function Project4Page() {
   return (
-    <div className="ml-0 md:ml-[var(--sidebar-width)] w-full h-[calc(100vh-8rem)] overflow-auto">
-      <HtmlRenderer 
-        filePath="/notebooks/wordcloud_Spanish.html" 
-        className="w-full h-full" 
-      />
+    <div className="w-full h-[calc(100vh-8rem)] overflow-auto">
+      <div className="max-w-4xl mx-auto p-4"> {/* Centering container */}
+        <HtmlRenderer 
+          filePath="/notebooks/wordcloud_Spanish.html" 
+          className="w-full" 
+        />
+      </div>
     </div>
   );
 }
