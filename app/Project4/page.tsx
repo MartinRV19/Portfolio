3015dbc7853ee73eed5ac97c646a1750b942bfac
Martin <rodriguezvalientemartin@gmail.com>
2025-04-28 18:22:50 -0300

    c2

--- app/Project4/page.tsx
+++ app/Project4/page.tsx
@@ -9,7 +9,7 @@ const HtmlRenderer = dynamic(
 
 export default function Project4Page() { 
     return (
-        <div className="h-full">
+        <div className="h-full overflow-hidden">
             <HtmlRenderer
                 filePath="/notebooks/wordcloud_Spanish.html"
                 className="w-full"
