680d99f9f48b9719616b259e890b55756bbe1fca
Martin <rodriguezvalientemartin@gmail.com>
2025-04-28 18:29:10 -0300

    c3

--- app/Project4/page.tsx
+++ app/Project4/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { useEffect } from "react";
 
 const HtmlRenderer = dynamic(
   () => import("@/components/html-renderer"),
@@ -8,6 +9,10 @@ const HtmlRenderer = dynamic(
 );
 
 export default function Project4Page() { 
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
         <div className="h-full overflow-hidden">
             <HtmlRenderer
